6823c5741a5080f77a93973329c79bfe06b705c9
CraneOfTheWild <arthur+github@m13g.com>
2019-06-06 13:26:11 +0200

    pie chart added

--- webApp/barChartV.js
+++ webApp/barChartV.js
@@ -1,28 +1,32 @@
 //  the data that powers the bar chart, a simple array of numeric values
-var chartData = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,20];
+var chartData = [1,0,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+
+var newChartData = [{"genre1": 4, "genre2": 8, "genre3": 1, "genre4": 0, "genre5": 7, "genre6": 15,}]
 
 //  the size of the overall svg element
 var width = 720,
     height = 200,
-    padding = {left:20,right:20};
+    padding = {left:20,right:20,top:10,bottom:30};
 
 var barWholeWidth = width / chartData.length,
-    barOffset = barWholeWidth/4,
-    barWidth = barWholeWidth-barOffset;
-
-var yScale = d3.scaleLinear()
-  .domain([0,d3.max(chartData)])
-  .range([0,height])
+    barOffset = barWholeWidth / 4,
+    barWidth = barWholeWidth - barOffset;
 
 var svg = d3.select('#bar-chart')
   .append('svg')
-  .attr('width', width)
-  .attr('height', height)
+  .attr('width', width + padding.left + padding.right)
+  .attr('height', height + padding.top + padding.bottom)
   .style('background', '#dff0d8')
+
+var yScale = d3.scaleLinear()
+  .domain([0,d3.max(chartData)])
+  .range([height, 0])
+
+var yAxis = d3.axisLeft()
+  .scale(yScale)
 
 svg.selectAll('rect').data(chartData)
   .enter().append('rect')
-//    .style({'fill': '#3c763d', 'stroke': '#d6e9c6', 'stroke-width': '5'})
   .attr('fill', '#3c763d')
   .attr('stroke', '#d6e9c6')
   .attr('stroke-width', '5')
@@ -30,11 +34,14 @@ svg.selectAll('rect').data(chartData)
     return barWidth;
   })
   .attr('height', function (data) {
-    return yScale(data);
+    return height - yScale(data);
   })
   .attr('x', function (data, i) {
-    return i * (barWidth + barOffset);
+    return i * (barWidth + barOffset) + 2 * padding.left;
   })
   .attr('y', function (data) {
-    return height - yScale(data);
+    return yScale(data) + padding.top;
   });
+
+svg.append("g").call(yAxis)
+  .attr("transform","translate(" + padding.left + "," + padding.top + ")");
