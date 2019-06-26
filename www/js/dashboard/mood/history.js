c44eb561c58e813ccacd5a9962cfac476e45b8a6
Jonasvdham <jonasvdham@gmail.com>
2019-06-26 18:36:45 +0200

    Update

--- www/js/dashboard/mood/history.js
+++ www/js/dashboard/mood/history.js
@@ -104,11 +104,11 @@ function resetFeedback(event) {
     var value = 50;
     document.getElementById("happiness_slider").value = value;
     var text = document.getElementById("happiness_slider_text");
-    text.textContent = "Happiness: " + value + "%";
+    text.textContent = value + "%";
 
     document.getElementById("excitedness_slider").value = value;
     var text = document.getElementById("excitedness_slider_text");
-    text.textContent = "Excitedness: " + value + "%";
+    text.textContent = value + "%";
 }
 
 // TODO call actual api with actual username
@@ -136,12 +136,12 @@ var tracklist = document.querySelector("#tracklist");
 
 document.getElementById("happiness_slider").oninput = function() {
     var text = document.getElementById("happiness_slider_text");
-    text.textContent = "Happiness: " + this.value + "%";
+    text.textContent = this.value + "%";
 };
 
 document.getElementById("excitedness_slider").oninput = function() {
     var text = document.getElementById("excitedness_slider_text");
-    text.textContent = "Excitedness: " + this.value + "%";
+    text.textContent = this.value + "%";
 };
 
 
@@ -151,103 +151,3 @@ resetButton.addEventListener("click", resetFeedback);
 var sendFeedbackBackButton = document.getElementById("send_feedback");
 sendFeedbackBackButton.addEventListener("click", sendFeedback);
 
-var songiddict = {};
-
-var request = new XMLHttpRequest()
-// request.open('GET', 'https://cors-anywhere.herokuapp.com/http://randomelements.nl/highmood/data/dummysonghistory.json', true)
-request.open('GET', 'https://cors-anywhere.herokuapp.com/http://pse-ssh.diallom.com:5000/api/tracks/history/' + "snipy12" + '/0', true)
-request.onload = function() {
-
-    if (request.status >= 200 && request.status < 400) {
-        var alldata = JSON.parse(this.response)
-        var userdata = alldata.resource
-        globaluserdata = userdata;
-        console.log(globaluserdata);
-        var songWidgetContainer = document.getElementById('song_history');
-        var form = document.createElement('form')
-
-        songWidgetContainer.appendChild(form);
-
-        var length = userdata.songs.length;
-
-        if (length > 20) {
-            length = 20;
-        }
-
-        for (var i = 0; i < length; i++) {
-
-            var song = userdata.songs[i];
-            var songdiv = document.createElement('div')
-            songdiv.classList.add('songdiv');
-            var songid = userdata.songs[i].songid;
-            songdiv.id = songid;
-
-            songiddict[songid] = song;
-
-            var btn = document.createElement("BUTTON");
-            btn.innerHTML = "Select";
-            btn.setAttribute("type", "button");
-            btn.onclick = function(songid) {
-                createSongInfoTool(songid);
-            };
-            btn.classList.add('SongRecButton');
-            songdiv.appendChild(btn);
-
-            var ifrm = document.createElement("iframe");
-            ifrm.setAttribute("src", "https://open.spotify.com/embed/track/" + userdata.songs[i].songid);
-            ifrm.setAttribute("align", "left");
-            ifrm.style.width = "300px";
-            ifrm.style.height = "80px";
-
-            songdiv.appendChild(ifrm);
-            form.appendChild(songdiv)
-        }
-
-        // createSongInfoTool(userdata.songs[0]);
-    }
-}
-request.send()
-
-function createSongInfoTool(clickevent) {
-
-    var songid = clickevent.path[0].parentElement.id;
-
-    console.log("Creating tool");
-    console.log(songid);
-    console.log(songiddict);
-
-    var song = songiddict[songid];
-    console.log(song);
-
-    // var songname = document.getElementById('songname');
-    // var par = document.createElement("p");
-    //
-    // var text = document.createTextNode(song.name);
-    // par.style.margin = "0px";
-    // par.appendChild(text);
-    // songname.appendChild(par);
-
-    var songname = document.getElementById('songdisplayname')
-    songname.innerHTML = song.name;
-
-    var happiness_slider = document.getElementById("happiness_slider")
-    var happiness_slider_text = document.getElementById("happiness_slider_text")
-    var happiness_percentage = (song.happiness + 10) * 5
-    happiness_slider.value = happiness_percentage;
-    happiness_slider_text.innerHTML = Math.trunc(happiness_percentage) + "%";
-
-
-    var excitedness_slider = document.getElementById("excitedness_slider")
-    var excitedness_slider_text = document.getElementById("excitedness_slider_text")
-    var excitedness_percentage = (song.excitedness + 10) * 5
-    excitedness_slider.value = excitedness_percentage;
-    excitedness_slider_text.innerHTML = Math.trunc(excitedness_percentage) + "%";
-
-
-    console.log(song.songid);
-    console.log(song.name);
-    console.log(song.excitedness);
-    console.log(song.happiness);
-
-    // songInfoContainer.appendChild(div);
-}
--- www/js/dashboard/recommendation/songrecommendation.js
+++ www/js/dashboard/recommendation/songrecommendation.js
@@ -59,49 +59,41 @@ function createSongRecommendationWidget(userid) {
   
   }
   
-  function showSong(clickevent) {
+function showSong(clickevent) {
     var songid = [clickevent.path[0].parentElement.id];
-  
-  
     var songdivs = document.getElementsByClassName("songdiv");
-  
+    var more_songs = document.getElementById("more_songs");
+    
     for (var i = 0; i < songdivs.length; i++) {
-  
-      if (songdivs[i].childNodes.length > 2) {
-  
-        for (var j = 2; j < songdivs[i].childNodes.length; j++) {
-          songdivs[i].removeChild(songdivs[i].childNodes[j]);
-        }
-      }
-  
+	if (songdivs[i].childNodes.length > 2) {
+	    
+            for (var j = 2; j < songdivs[i].childNodes.length; j++) {
+		songdivs[i].removeChild(songdivs[i].childNodes[j]);
+            }
+	}
     }
-  
+    
     var request = new XMLHttpRequest()
-  
+    
     request.open('GET', '/api/tracks/recommendation/' + userid + '/' + songid + '/0.0/0.0', true)
     // /api/tracks/recommendation/snipy12/0LtOwyZoSNZKJWHqjzADpW/0.0/0.0
     request.onload = function() {
-      var alldata = JSON.parse(this.response)
-      var userdata = alldata.resource
-  
-      if (request.status >= 200 && request.status < 400) {
-  
-        songdivs = document.getElementsByClassName("songdiv");
-  
-        for (var i = 0; i < songdivs.length; i++) {
-  
-  
-          var ifrm = document.createElement("iframe");
-          ifrm.setAttribute("src", "https://open.spotify.com/embed/track/" + userdata.recommendations[i].songid);
-          ifrm.setAttribute("align","left");
-          ifrm.style.width = "300px";
-          ifrm.style.height = "80px";
-  
-          songdivs[i].appendChild(ifrm)
-        }
-  
-      }
+	console.log("response")
+	console.log(this.response)
+	var alldata = JSON.parse(this.response)
+	var userdata = alldata.resource
+	
+	if (request.status >= 200 && request.status < 400) {
+            songdivs = document.getElementsByClassName("songdiv");
+	    for (var i = 0; i < 5; i++) {
+		var ifrm = document.createElement("iframe");
+		ifrm.setAttribute("src", "https://open.spotify.com/embed/track/" + userdata.recommendations[i].songid);
+		ifrm.setAttribute("align","left");
+		ifrm.style.width = "300px";
+		ifrm.style.height = "80px";
+		more_songs.appendChild(ifrm)
+            }
+	}
     }
     request.send()
-  
-  }
+}
