97bad6fae83313f9e1148f78b2b7a7f654db9977
HNegus <henok.ghebrenigus@eyp.nl>
2019-06-26 14:19:30 +0200

    add reset event listener for feedback sliders

--- www/js/dashboard/mood/history.js
+++ www/js/dashboard/mood/history.js
@@ -10,10 +10,22 @@ function update_history() {
     }
 }
 
+// TODO  set to actual song feedbacl
+function resetFeedback(event) {
+    var value = 50;
+
+    document.getElementById("happiness_slider").value = value;
+    var text = document.getElementById("happiness_slider_text");
+    text.textContent = "Happiness: " + value + "%";
+
+    document.getElementById("excitedness_slider").value = value;
+    var text = document.getElementById("excitedness_slider_text");
+    text.textContent = "Excitedness: " + value + "%";
+}
+
 var song_history = document.querySelector("#song_history");
 var tracklist = document.querySelector("#tracklist");
 update_history();
-
 
 document.getElementById("happiness_slider").oninput = function () {
     var text = document.getElementById("happiness_slider_text");
@@ -24,3 +36,7 @@ document.getElementById("excitedness_slider").oninput = function () {
     var text = document.getElementById("excitedness_slider_text");
     text.textContent = "Excitedness: " + this.value + "%";
 };
+
+
+var resetButton = document.getElementById("reset_feedack");
+resetButton.addEventListener("click", resetFeedback)
