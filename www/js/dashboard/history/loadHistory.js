083a5250c029015bbcc2a7adb37d68fe139b08f0
barry.de.vries123@gmail.com <barry.de.vries123@gmail.com>
2019-06-27 10:28:08 +0200

    fixed bug with topsongs not loading

--- www/js/dashboard/history/loadHistory.js
+++ www/js/dashboard/history/loadHistory.js
@@ -1,41 +1,34 @@
 // var userid = document.getElementById("username").textContent;
-// console.log(userid);
-$('#favCol').hide();
 // TODO remove hardcode
 var userid = 'snipy12';
 
+// Show history data when websites is opened.
 var request = new XMLHttpRequest();
+request.open('GET', 'http://localhost:5000/api/tracks/history/' + userid + '/0', true);
 
-request.open('GET', 'http://localhost:5000/api/tracks/history/' + userid + '/0', true)
 request.onload = function() {
     var allData = JSON.parse(this.response);
     userData = allData.resource.songs;
-    window.userData = userData;
-    console.log(request.status, "request status!")
-    if (request.status >= 200 && request.status < 400) {
-        // Song history.
+    window.histData = userData;
+    window.curData = window.histData;
+    
+    if (request.status == 200) {
         createHistory(userData);
     }
 }
 request.send();
 
+// Onclick handles for toggle.
 function toggleHistory(chartname) {
     if (chartname === 'history') {
         $('#historySelector').text("History ");
         $('#historySelector').append("<span class=\"caret\"></span>");
-        $('#histCol').show();
-        $('#favCol').hide();
-    }
-    else if (chartname === 'favourites') {
+        window.curData = window.histData;
+        createHistory(window.histData);
+    } else if (chartname === 'favourites') {
         $('#historySelector').text("Favourites ");
-        $('#historySelector').append("<span class=\"caret\"></span>");
-        topdata = getTopData();
-        if ($('favRow').children().length == 0) {
-            // Favourites.
-            createTop(topdata);
-        }
-        $('#histCol').hide();
-        $('#favCol').show();
+        $('#historySelector').append("<span class=\"caret\"></span>"); // TODO ask Arthus what this does
+        getTopData(); // if not top data?
     }
 }
 
@@ -47,9 +40,11 @@ function getTopData() {
         var allTopData = JSON.parse(this.response);
         userTopData = allTopData.resource.songs;
         window.userTopData = userTopData;
-        console.log(userTopData);
-        if (topRequest.status >= 200 && topRequest.status < 400) {
-            return userTopData;
+        
+        if (topRequest.status == 200) {
+            window.topData = userTopData;
+            window.curData = window.topData;
+            createTop();
         } else {
             document.getElementById("userwelcome").innerHTML = "Error retrieving data!";
         }
@@ -57,35 +52,39 @@ function getTopData() {
     topRequest.send();
 }
 
-function createHistory(userData) {
-  for (var index = 0; index < Math.min(5, userData.length); index++) {
+function createHistory() {
+  for (var index = 0; index < Math.min(5, window.curData.length); index++) {
     div = $('#hist' + index);
     div.empty();
     trackId = "https://open.spotify.com/embed/track/";
-    trackId += userData[index].songid;
+    trackId += window.curData[index].songid;
 
     content = '<iframe class="song-template" src="' + trackId + '" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>';
     div.append(content);
     }
 }
 
-function createTop(topData) {
+// make createHistory so that we don't need two functions? They should do the exact same thing afterall.
+function createTop() {
 }
 
-// TODO: test
 function select(song_index) {
-    songId = window.userData[parseInt(song_index)].songid   ;
+    console.log("logging topData");
+    console.log(window.topData);
+    console.log("logging curData");
+    console.log(window.curData);
+    songId = window.curData[parseInt(song_index)].songid;
     
-    var requestRec = new XMLHttpRequest();
-    requestRec.open('GET', 'http://localhost:5000/api/tracks/recommendation/' + userid + '/' + songId + '/0.0/0.0', true);
-    requestRec.onload = function() {
+    var recRequest = new XMLHttpRequest();
+    recRequest.open('GET', 'http://localhost:5000/api/tracks/recommendation/' + userid + '/' + songId + '/0.0/0.0', true);
+    recRequest.onload = function() {
         var data = JSON.parse(this.response);
         console.log(data)
         var recommendations = data.resource.recommendations;
         for (var index = 0; index < 5; index++) {
             div = $('#recHist' + index);
             div.empty();
-            if (requestRec.status == 200) {
+            if (recRequest.status == 200) {
                 trackId = "https://open.spotify.com/embed/track/";
                 trackId += recommendations[index].songid;
             
@@ -96,5 +95,5 @@ function select(song_index) {
             }
         }
     }
-    requestRec.send();
+    recRequest.send();
 }
