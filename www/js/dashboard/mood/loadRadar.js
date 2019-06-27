3e7e95d0536cb46db627c66d71bba0aca39ae167
CraneOfTheWild <arthur+github@m13g.com>
2019-06-27 12:55:53 +0200

    commented loadRadar.js

--- www/js/dashboard/mood/loadRadar.js
+++ www/js/dashboard/mood/loadRadar.js
@@ -1,12 +1,10 @@
-
-
+// Function to create the radarchart from the userdata.
 function createRadarChart(userdata) {
 
   /* Radar chart design created by Nadieh Bremer - VisualCinnamon.com */
   var margin = {top: 100, right: 100, bottom: 100, left: 100},
     width = Math.min(700, window.innerWidth - 10) - margin.left - margin.right,
     height = Math.min(width, window.innerHeight - margin.top - margin.bottom - 20);
-
 
   var color = d3.scaleOrdinal().range(["#EDC951","#EDC951"])
 
@@ -23,11 +21,9 @@ function createRadarChart(userdata) {
     color: color
   };
 
-
-  var finaldata = [];
+  var finalData = [];
   var song;
   for (i = 0; i < userdata.songs.length; i++) {
-
     song = {
       name : 'Song',
       axes : [
@@ -37,9 +33,9 @@ function createRadarChart(userdata) {
         {axis:"Low Happiness",value:0.0}
       ]
     }
-    finaldata.push(song);
+    finalData.push(song);
   }
 
-  let svg_radar1 = RadarChart(".radarChart", finaldata, radarChartOptions);
+  let svg_radar1 = RadarChart(".radarChart", finalData, radarChartOptions);
 }
 
