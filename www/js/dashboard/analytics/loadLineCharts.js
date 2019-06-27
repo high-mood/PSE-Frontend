77ef482f6aaa7b503029b9a233984313eeb034b2
jellewe <jellewe@live.nl>
2019-06-27 15:23:15 +0200

    barchart API call implemented

--- www/js/dashboard/analytics/loadLineCharts.js
+++ www/js/dashboard/analytics/loadLineCharts.js
@@ -49,6 +49,32 @@ function requestLineCharts(retriggered) {
     }
   }
   linechartDaysRequest.send()
+
+
+
+  var barchartRequest = new XMLHttpRequest()
+
+  // var metrics = "acousticness, danceability, duration_ms, energy, instrumentalness, key, liveness, loudness, mode, speechiness, tempo, valence";
+  // linechartDaysRequest.open('GET', 'https://cors-anywhere.herokuapp.com/http://randomelements.nl/highmood/data/api_days_dummy.json', true)
+  var start = 12;
+  var end = 20;
+  barchartRequest.open('GET', 'http://localhost:5000/api/user/mood/hourly/' + userid + '/' + start + '/' + end, true)
+  barchartRequest.onload = function() {
+    var alldata = JSON.parse(this.response)
+    var userdata = alldata.resource
+
+    console.log("alldata: " + alldata)
+    console.log(userdata)
+    if (barchartRequest.status >= 200 && barchartRequest.status < 400) {
+      // lineGraph
+      // createLineGraphDays(userdata,"lineDays", retriggered);
+      // giveText(userdata,"lineGraphText");
+    }
+    else {
+      document.getElementById("userwelcome").innerHTML = "Error retrieving data!"
+    }
+  }
+  barchartRequest.send()
 }
 
 requestLineCharts(false)
